fdbe2f95e7918ba080d8a86b35f3329eb643ffb6
xanthian <xanneh@hotmail.com>
2022-01-01 22:15:23 +0000

    Update ShroomlightSlabs.java
    dfsafest432t34

--- src/main/java/net/xanthian/shroomlight_slab/ShroomlightSlabs.java
+++ src/main/java/net/xanthian/shroomlight_slab/ShroomlightSlabs.java
@@ -1,8 +1,7 @@
-package net.xanthian.shroomlight_slab;
-
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
+import net.minecraft.block.Material;
 import net.minecraft.block.SlabBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
@@ -11,18 +10,13 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 
+
 public class ShroomlightSlabs implements ModInitializer {
-	private static final String MOD_ID = "shroomlight_slab";
-	private static final float HARDNESS = 1.0f;
-	private static final float RESISTANCE = 1.0f;
-	
-	
     public static final Identifier namespace = new Identifier("shroomlight_slab", "shroomlight_slab");
 
-    public static final Block SHROOMLIGHT_SLAB = SlabBlock(FabricBlockSettings.strength(HARDNESS, RESISTANCE));
-	
+    public static final Block SHROOMLIGHT_SLAB = Registry.register(Registry.BLOCK, namespace, new SlabBlock(FabricBlockSettings.of(Material.NETHER_SHOOTS).strength(1.0F, 1.0F)));
     public static final Item SHROOMLIGHT_SLAB_ITEM = Registry.register(Registry.ITEM, namespace, new BlockItem(SHROOMLIGHT_SLAB, new Item.Settings().group(ItemGroup.MISC)));
 
     @Override
-	public void onInitialize() {}
+    public void onInitialize() {}
 }
