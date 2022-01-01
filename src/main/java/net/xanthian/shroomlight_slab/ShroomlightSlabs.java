f6daaa1ea5209636e1376209381252c9f31b030d
xanthian <xanneh@hotmail.com>
2022-01-01 23:43:54 +0000

    Update ShroomlightSlabs.java

--- src/main/java/net/xanthian/shroomlight_slab/ShroomlightSlabs.java
+++ src/main/java/net/xanthian/shroomlight_slab/ShroomlightSlabs.java
@@ -4,7 +4,6 @@ package net.xanthian.shroomlight_slab;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricMaterialBuilder;
-import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
 import net.minecraft.block.Block;
 import net.minecraft.block.MapColor;
 import net.minecraft.block.SlabBlock;
@@ -14,14 +13,12 @@ import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
-import static net.minecraft.block.piston.PistonBehavior.BLOCK;
-
 
 public class ShroomlightSlabs implements ModInitializer {
     public static final Identifier namespace = new Identifier("shroomlight_slab", "shroomlight_slab");
 
-    public static final Block SHROOMLIGHT_SLAB = Registry.register(Registry.BLOCK, namespace, new SlabBlock(FabricBlockSettings.of((new FabricMaterialBuilder(MapColor.BLACK)).pistonBehavior(BLOCK).build()).requiresTool().breakByTool(FabricToolTags.PICKAXES, 3).strength(1.0F, 1.0F)));
-    public static final Item SHROOMLIGHT_SLAB_ITEM = Registry.register(Registry.ITEM, namespace, new BlockItem(SHROOMLIGHT_SLAB, new Item.Settings().group(ItemGroup.REDSTONE)));
+    public static final Block SHROOMLIGHT_SLAB = Registry.register(Registry.BLOCK, namespace, new SlabBlock(FabricBlockSettings.of((new FabricMaterialBuilder(MapColor.BLACK)).build()).strength(1.0F, 1.0F)));
+    public static final Item SHROOMLIGHT_SLAB_ITEM = Registry.register(Registry.ITEM, namespace, new BlockItem(SHROOMLIGHT_SLAB, new Item.Settings().group(ItemGroup.MISC)));
 
     @Override
 	public void onInitialize() {}
