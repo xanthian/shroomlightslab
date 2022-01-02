586c33790a6c3ead747d81cfafe0e5a8be47951d
xanthian <xanneh@hotmail.com>
2022-01-02 00:39:35 +0000

    Update ShroomlightSlabs.java

--- src/main/java/net/xanthian/shroomlight_slab/ShroomlightSlabs.java
+++ src/main/java/net/xanthian/shroomlight_slab/ShroomlightSlabs.java
@@ -15,9 +15,9 @@ import net.minecraft.util.registry.Registry;
 public class ShroomlightSlabs implements ModInitializer {
     public static final Identifier namespace = new Identifier("shroomlight_slab", "shroomlight_slab");
 
-    public static final Block SHROOMLIGHT_SLAB = Registry.register(Registry.BLOCK, namespace, new SlabBlock(FabricBlockSettings.of(Material.GRASS).strength(1.0f, 1.0f).luminance(15)));
+    public static final Block SHROOMLIGHT_SLAB = Registry.register(Registry.BLOCK, namespace, new SlabBlock(FabricBlockSettings.of(Material.DECORATION).requiresTool().strength(1.0f, 1.0f).luminance(15)));
     public static final Item SHROOMLIGHT_SLAB_ITEM = Registry.register(Registry.ITEM, namespace, new BlockItem(SHROOMLIGHT_SLAB, new Item.Settings().group(ItemGroup.DECORATIONS)));
 
     @Override
-	public void onInitialize() {}
+    public void onInitialize() {}
 }
