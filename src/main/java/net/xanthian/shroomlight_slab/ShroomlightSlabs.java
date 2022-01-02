ab2392ca27cf308331ab7c0d0b832fa68c1887af
xanthian <xanneh@hotmail.com>
2022-01-02 00:48:28 +0000

    Update ShroomlightSlabs.java
    trying something different

--- src/main/java/net/xanthian/shroomlight_slab/ShroomlightSlabs.java
+++ src/main/java/net/xanthian/shroomlight_slab/ShroomlightSlabs.java
@@ -1,21 +1,22 @@
 package net.xanthian.shroomlight_slab;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.SlabBlock;
-import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
+import static net.minecraft.block.Blocks.*;
+
 
 public class ShroomlightSlabs implements ModInitializer {
     public static final Identifier namespace = new Identifier("shroomlight_slab", "shroomlight_slab");
 
-    public static final Block SHROOMLIGHT_SLAB = Registry.register(Registry.BLOCK, namespace, new SlabBlock(FabricBlockSettings.of(Material.DECORATION).requiresTool().strength(1.0f, 1.0f).luminance(15)));
+    public static final Block SHROOMLIGHT_SLAB = Registry.register(Registry.BLOCK, namespace, new SlabBlock(AbstractBlock.Settings.copy(SHROOMLIGHT)));
     public static final Item SHROOMLIGHT_SLAB_ITEM = Registry.register(Registry.ITEM, namespace, new BlockItem(SHROOMLIGHT_SLAB, new Item.Settings().group(ItemGroup.DECORATIONS)));
 
     @Override
