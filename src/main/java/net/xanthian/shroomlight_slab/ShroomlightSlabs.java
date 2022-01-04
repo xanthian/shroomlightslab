e99f91107a41b59ba63add94a878db5a0e208b52
xanthian <xanneh@hotmail.com>
2022-01-04 18:10:10 +0000

    few more updates.

--- src/main/java/net/xanthian/shroomlight_slab/ShroomlightSlabs.java
+++ src/main/java/net/xanthian/shroomlight_slab/ShroomlightSlabs.java
@@ -1,24 +1,15 @@
 package net.xanthian.shroomlight_slab;
 
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Block;
-import net.minecraft.block.SlabBlock;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
-
-import static net.minecraft.block.Blocks.*;
-
+import net.xanthian.shroomlight_slab.block.Slabs;
 
 public class ShroomlightSlabs implements ModInitializer {
-    public static final Identifier namespace = new Identifier("shroomlight_slab", "shroomlight_slab");
 
-    public static final Block SHROOMLIGHT_SLAB = Registry.register(Registry.BLOCK, namespace, new SlabBlock(AbstractBlock.Settings.copy(SHROOMLIGHT).strength(0.2f)));
-    public static final Item SHROOMLIGHT_SLAB_ITEM = Registry.register(Registry.ITEM, namespace, new BlockItem(SHROOMLIGHT_SLAB, new Item.Settings().group(ItemGroup.DECORATIONS)));
+    public static final String MOD_ID = "shroomlight_slab";
 
     @Override
-    public void onInitialize() {}
+    public void onInitialize() {
+        Slabs.registerModBlocks();
+        System.out.println("Shroomlight Slabs loaded");
+}
 }
---
+++ src/main/java/net/xanthian/shroomlight_slab/block/Slabs.java
@@ -0,0 +1,42 @@
+package net.xanthian.shroomlight_slab.block;
+
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.*;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.item.Item;
+import net.minecraft.util.registry.Registry;
+import net.xanthian.shroomlight_slab.ShroomlightSlabs;
+
+//Slabs
+public class Slabs {
+
+    public static final Block SHROOMLIGHT_SLAB = registerBlock("shroomlight_slab",
+            new SlabBlock(FabricBlockSettings.copy(Blocks.SHROOMLIGHT).nonOpaque()));
+
+    public static final Block GLOWSTONE_SLAB = registerBlock("glowstone_slab",
+            new SlabBlock(FabricBlockSettings.copy(Blocks.GLOWSTONE).nonOpaque()));
+
+
+//Register Blocks
+    private static Block registerBlock(String name, Block block){
+        registerBlockItem(name, block);
+        return Registry.register(Registry.BLOCK, new Identifier(ShroomlightSlabs.MOD_ID, name), block);
+    }
+
+//Register Items
+    private static Item registerBlockItem(String name, Block block){
+        return Registry.register(Registry.ITEM, new Identifier(ShroomlightSlabs.MOD_ID, name),
+                new BlockItem(block, new Item.Settings().group(ItemGroup.DECORATIONS)));
+    }
+
+//Console Print
+    public static void registerModBlocks(){
+        System.out.println("Registering Blocks for "+ ShroomlightSlabs.MOD_ID);
+
+
+    }
+
+
+}
